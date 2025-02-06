adbe1a664ade1cdc11ca13c855f70de4f6fa56b4
Francisco <franciscodavi.s.costa@gmail.com>
2025-02-06 13:06:42 -0300

    Desafio Resolvido

--- Program.cs
+++ Program.cs
@@ -7,19 +7,26 @@ Console.OutputEncoding = Encoding.UTF8;
 List<Pessoa> hospedes = new List<Pessoa>();
 
 Pessoa p1 = new Pessoa(nome: "Hóspede 1");
-Pessoa p2 = new Pessoa(nome: "Hóspede 2");
+//Pessoa p2 = new Pessoa(nome: "Hóspede 2");
+
 
 hospedes.Add(p1);
-hospedes.Add(p2);
+//hospedes.Add(p2);
 
 // Cria a suíte
-Suite suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 30);
+Suite suite = new Suite(tipoSuite: "Premium", capacidade: 5, valorDiaria: 30);
 
 // Cria uma nova reserva, passando a suíte e os hóspedes
-Reserva reserva = new Reserva(diasReservados: 5);
-reserva.CadastrarSuite(suite);
-reserva.CadastrarHospedes(hospedes);
+try{
+    Reserva reserva = new Reserva(diasReservados: 1);
+    reserva.CadastrarSuite(suite);
+    reserva.CadastrarHospedes(hospedes);
 
-// Exibe a quantidade de hóspedes e o valor da diária
-Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
-Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria()}");
+    // Exibe a quantidade de hóspedes e o valor da diária
+    Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
+    Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria()}");
+}catch(Exception e){
+    Console.WriteLine("Erro ao realizar a reserva: " + e.Message);
+}
+
+
